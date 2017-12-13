447bc82f47d47e699369902f61a50509845e4b2f
Lukáš Stankovič <lukasstankovic@gmail.com>
2017-12-13 10:32:39 +0100

    Part2 - admin + db

--- mysite/travel_agency/models.py
+++ mysite/travel_agency/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    birthdate = models.DateField('birthdate')
+    people = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " " + self.surname
+
+class Destination(models.Model):
+    city = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.city
+
+class Excursion(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+    datetime = models.DateTimeField('excursion date')
+    desc = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.desc
+
